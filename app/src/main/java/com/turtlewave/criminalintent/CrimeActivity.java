7edea49995b7c7ced53b7472251a2d24398c7111
TJDosunmu <theblasiantj@gmail.com>
2018-03-27 17:47:15 -0400

    inital commit

---
+++ app/src/main/java/com/turtlewave/criminalintent/CrimeActivity.java
@@ -0,0 +1,17 @@
+package com.turtlewave.criminalintent;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Jay on 3/27/2018.
+ */
+
+public class CrimeActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
